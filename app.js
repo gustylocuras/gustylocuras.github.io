ef3cfc43bb787381b279a1dc1d3fe10183cdcf95
Agustin Alvarez <agustinalvarez@agustins-mbp.home>
2020-05-15 20:35:28 -0700

    started carousel

--- app.js
+++ app.js
@@ -2,21 +2,19 @@
   let token = ''
   let difficulty = ''
 
-  $('.difficulty').on('click', (event) => {
-    difficulty = $(event.currentTarget).text().toLowerCase()
-    $('#modal').css('display', 'none')
-  })
-
   $.ajax({
 
-   	"url": "https://opentdb.com/api_token.php?command=request"
+    "url": "https://opentdb.com/api_token.php?command=request"
 
   }).then((data)=> {
      token = data.token;
      console.log(token);
   });
 
-  $('#start').on('click', (event) => {
+
+  $('.difficulty').on('click', (event) => {
+    difficulty = $(event.currentTarget).text().toLowerCase()
+    $('#modal').css('display', 'none')
 
     $.ajax({
 
@@ -24,6 +22,17 @@
 
     }).then((data)=> {
        console.log(data);
+       for(let question of data.results){
+         const $questionBox = $('<div>').addClass('question-box').appendTo('.question-container')
+         const $category =
+                  $('<h4>')
+                  .addClass('category')
+                  .text(question.category)
+                  .appendTo($questionBox)
+
+       }
+
+
 
     });
   })
