d5833a75a87da6a5c15713f23918555cf2bf4321
Agustin Alvarez <agustinalvarez@agustins-mbp.home>
2020-05-18 10:24:52 -0700

    sizing

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const start = function (){
 
   }).then((data)=> {
      token = data.token;
-     console.log(token);
   });
 
 
@@ -33,7 +32,6 @@ const start = function (){
      	"url": "https://opentdb.com/api.php?amount=10&token=" + token + "&difficulty=" + difficulty,
 
     }).then((data)=> {
-       console.log(data);
        for(let question of data.results){
          const $questionBox = $('<div>').addClass('question-box').appendTo('.question-container')
          const $category =
